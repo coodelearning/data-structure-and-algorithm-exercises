6aa296143a1efbf44276ada4786034c7ad66afe6
王梓逻 <q102q103q104@163.com>
2020-05-29 18:39:01 +0800

    feat(tiqe): add array 27 solution

--- golang/top-interview-questions-easy/1/array/27/27.go
+++ golang/top-interview-questions-easy/1/array/27/27.go
@@ -1,21 +1,50 @@
 package _27
 
+import "log"
+
 func plusOne(digits []int) []int {
+	// 空数组直接结束
 	if len(digits) == 0 {
 		return nil
 	}
 	var result []int
-	for _, v := range digits {
-		if v == 9 {
+	// 如果为9直接返回10
+	if len(digits) == 1 && digits[0] == 9 {
+		return []int{1, 0}
+	}
+	for k, v := range digits {
+		// 判断末尾是不是9
+		if v == 9 && k == len(digits)-1 {
+			// 是9的化开始进位操作
 			goto next
 		}
 	}
+	// 不是9的话常规末尾加一就可以返回了
 	result = digits
-	result[len(digits)-1] = result[len(digits)-1] + 1
+	result[len(result)-1] = result[len(result)-1] + 1
 	return result
 next:
-	for i := len(digits) - 1; i >= 0; i-- {
-
+	result = digits
+	log.Println("result", result)
+	for i := len(result) - 1; i >= 0; i-- {
+		// 末尾为9,进位
+		if result[i] >= 9 && i > 0 {
+			result[i-1] = result[i-1] + 1
+			result[i] = 0
+			// 如果进位之后高位没有到10则可以结束了
+			// 若高位为10继续进位
+			if result[i-1] != 10 {
+				break
+			}
+		}
 	}
-	return nil
+	// 如果最高位为10 则添加一位,将其后的值补入结果
+	if result[0] == 10 {
+		result[0] = 0
+		var newResult []int
+		newResult = append(newResult, 1)
+		newResult = append(newResult, result...)
+		return newResult
+	}
+	return result
 }
--- golang/top-interview-questions-easy/1/array/27/27_test.go
+++ golang/top-interview-questions-easy/1/array/27/27_test.go
@@ -24,9 +24,30 @@ func Test_plusOne(t *testing.T) {
 		{
 			name: "2",
 			args: args{
-				digits: []int{4, 3, 2, 1},
+				digits: []int{9},
 			},
-			want: []int{4, 3, 2, 2},
+			want: []int{1, 0},
+		},
+		{
+			name: "2.1",
+			args: args{
+				digits: []int{9, 9, 9},
+			},
+			want: []int{1, 0, 0, 0},
+		},
+		{
+			name: "3",
+			args: args{
+				digits: []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+			},
+			want: []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 1},
+		},
+		{
+			name: "4",
+			args: args{
+				digits: []int{2, 4, 9, 3, 9},
+			},
+			want: []int{2, 4, 9, 4, 0},
 		},
 	}
 	for _, tt := range tests {
