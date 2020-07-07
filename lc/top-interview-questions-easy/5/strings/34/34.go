0decf89f5329494c1bb86d2e18c5656fa87deac0
王梓逻 <q102q103q104@163.com>
2020-07-07 17:58:50 +0800

    feat(toqe): Add strings 35

--- lc/top-interview-questions-easy/5/strings/34/34.go
+++ lc/top-interview-questions-easy/5/strings/34/34.go
@@ -1,6 +1,41 @@
 package _34
 
-func firstUniqChar(s string) int {
+import (
+	"log"
+	"strings"
+)
 
-	return 0
+func firstUniqChar(s string) int {
+	charS := strings.Split(s, "")
+	if len(charS) == 0 {
+		return -1
+	}
+	if len(charS) == 1 {
+		return 0
+	}
+	log.Println(charS)
+	mapS := make(map[string]int)
+	var repeatCharIndex []int
+	for index, char := range charS {
+		_, ok := mapS[char]
+		if ok {
+			repeatCharIndex = append(repeatCharIndex, mapS[char], index)
+		} else {
+			mapS[char] = index
+		}
+	}
+	log.Println("The repCI is :", repeatCharIndex)
+	for i := 0; i < len(charS); i++ {
+		isRepat := false
+		for _, v := range repeatCharIndex {
+			if i == v {
+				isRepat = true
+				break
+			}
+		}
+		if !isRepat {
+			return i
+		}
+	}
+	return -1
 }
