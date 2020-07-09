28186ad7a43f7538208b0b95a52dc84eb5d84cf6
FWangZil <acewzk@gmail.com>
2020-07-09 19:32:06 +0800

    feat(toqe): Add strings 40 (200709-3)

--- lc/top-interview-questions-easy/5/strings/40/40.go
+++ lc/top-interview-questions-easy/5/strings/40/40.go
@@ -1,8 +1,57 @@
 package _40
 
+import (
+	"fmt"
+	"log"
+)
+
 // 所有输入只包含小写字母 `a-z` 。
 
 func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
 
-	return ""
+	if len(strs) == 1 {
+		return strs[0]
+	}
+
+	var index int
+
+	for j := 0; j < len(strs[0]); j++ {
+		if index != 0 {
+			break
+		}
+		for i := 1; i < len(strs); i++ {
+			if j > len(strs[i])-1 {
+				index = j - 1
+				break
+			}
+			preChar := strs[0][j]
+			// TODO:缺少了兼容完全匹配的逻辑
+			if j < len(strs[i]) && strs[i][j] != preChar {
+				if j == 0 && len(strs[i]) != 1 {
+					log.Println("Im return in here 1 and len(strs[i]) =", len(strs[i]))
+					return ""
+				}
+				index = j
+				break
+			}
+			if j == len(strs[i])-1 {
+				index = len(strs[0])
+				break
+			}
+		}
+	}
+
+	if index < 0 {
+		log.Println("Im return in here 2")
+		return ""
+	}
+
+	var result string
+	for i := 0; i < index; i++ {
+		result = fmt.Sprintf("%s%s", result, string(strs[0][i]))
+	}
+	return result
 }
--- lc/top-interview-questions-easy/5/strings/40/40_test.go
+++ lc/top-interview-questions-easy/5/strings/40/40_test.go
@@ -25,11 +25,39 @@ func Test_longestCommonPrefix(t *testing.T) {
 			},
 			want: "",
 		},
+		{
+			name: "测试3",
+			args: args{
+				strs: []string{"a"},
+			},
+			want: "a",
+		},
+		{
+			name: "测试4",
+			args: args{
+				strs: []string{"c", "c"},
+			},
+			want: "c",
+		},
+		{
+			name: "测试5",
+			args: args{
+				strs: []string{"aa", "aa"},
+			},
+			want: "aa",
+		},
+		{
+			name: "测试6",
+			args: args{
+				strs: []string{"aa", "a"},
+			},
+			want: "a",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := longestCommonPrefix(tt.args.strs); got != tt.want {
-				t.Errorf("longestCommonPrefix() = %v, want %v", got, tt.want)
+				t.Errorf("longestCommonPrefix(%v) = %v, want %v", tt.args.strs, got, tt.want)
 			}
 		})
 	}
