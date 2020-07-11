1c040743d2522872f6ccda5d68e87a2f069bd47b
FWangZil <acewzk@gmail.com>
2020-07-11 16:49:09 +0800

    feat(tiqe/trees): Finish 47 (20200711-3)

--- lc/top-interview-questions-easy/trees/47/47.go
+++ lc/top-interview-questions-easy/trees/47/47.go
@@ -7,6 +7,41 @@ type TreeNode struct {
 }
 
 func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
 
-	return 0
+	var leftLen = 1
+	var rightLen = 1
+
+	if root.Left != nil {
+		leftLen += maxDepth(root.Left)
+	}
+	if root.Right != nil {
+		rightLen += maxDepth(root.Right)
+	}
+
+	if leftLen > rightLen {
+		return leftLen
+	}
+
+	return rightLen
+}
+
+func maxDepthBetter(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	// divide：分左右子树分别计算
+	left := maxDepthBetter(root.Left)
+	right := maxDepthBetter(root.Right)
+
+	// conquer：合并左右子树结果
+	if left > right {
+		return left + 1
+	}
+	return right + 1
 }
