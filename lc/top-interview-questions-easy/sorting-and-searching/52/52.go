b3af75558ba2fc1633596b4597202e5e16a10530
FWangZil <acewzk@gmail.com>
2020-07-16 15:41:59 +0800

    feat(tiqe/trees): Finish 51 (20200716-1)

---
+++ lc/top-interview-questions-easy/sorting-and-searching/52/52.go
@@ -0,0 +1,5 @@
+package _52
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+
+}
--- lc/top-interview-questions-easy/trees/51/51.go
+++ lc/top-interview-questions-easy/trees/51/51.go
@@ -1,12 +1,36 @@
 package _51
 
+import (
+	"math/rand"
+	"time"
+)
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
-func sortedArrayToBST(nums []int) *TreeNode {
+// 二叉搜索树的中序遍历是升序序列.
+// 题目给定的数组是按照升序排序的有序数组，因此可以确保数组是二叉搜索树的中序遍历序列。
 
-	return nil
+func sortedArrayToBST(nums []int) *TreeNode {
+	// 方法三
+	rand.Seed(time.Now().UnixNano())
+	return helper(nums, 0, len(nums)-1)
+}
+
+func helper(nums []int, leftIndex, rightIndex int) *TreeNode {
+	if leftIndex > rightIndex {
+		return nil
+	}
+	// 方法一：中序遍历，总是选择中间位置左边的数字作为根节点
+	//midIndex := (leftIndex + rightIndex) / 2
+	// 方法二：中序遍历，总是选择中间位置右边的数字作为根节点
+	// 选择任意一个中间位置数字作为根节点
+	midIndex := (leftIndex + rightIndex + rand.Intn(2)) / 2
+	result := &TreeNode{Val: nums[midIndex]}
+	result.Left = helper(nums, leftIndex, midIndex-1)
+	result.Right = helper(nums, midIndex+1, rightIndex)
+	return result
 }
